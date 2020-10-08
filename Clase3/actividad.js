5f37bea4f9d80d19f72bb13666047048248f2b58
Gustavo Maldonado <gamtt1994@gmail.com>
2020-10-07 20:54:26 -0500

    Actividad clase 3

---
+++ Clase3/actividad.js
@@ -0,0 +1,51 @@
+/*
+Crear un arreglo
+*/
+
+console.log("-- Ejercicio 1 - creando un array");
+let pokemon = ["pikachu", "raichu", "charizard", "tauros"];
+console.log(pokemon);
+console.log("-- Fin Ejercicio 1");
+console.log("-");
+/*
+Añadir un elemento al final o otro al inicio del arreglo
+*/
+console.log("-");
+console.log("--Ejercicio 2 - añadiendo valores al inicio y al final");
+pokemon.unshift("tangela");
+pokemon.push("mega Charizard");
+console.log(pokemon);
+
+console.log("-- Fin Ejercicio 2 - añadiendo valores al inicio y al final");
+console.log("-");
+/*/
+Concatenenar dos arreglos
+*/
+console.log("-");
+console.log("--Ejercicio 3 - uniendo array");
+let digimon = ["agumon", "patamon"];
+let union = pokemon.concat(digimon);
+console.log("-- Fin Ejercicio 3");
+/*
+retornar los elementos de un arreglo (for o for of)
+*/
+console.log("--Ejercicio 4 - recorrer arreglo");
+for (i = 0; i < union.length; i++) {
+  console.log(i, union[i]);
+}
+console.log("--Fin Ejercicio 4 - ");
+console.log("-");
+/*
+Ordenar y listar alfabeticamente un arreglo
+*/
+console.log("--Ejercicio 5 - ordenar");
+pokemon.sort();
+console.log(pokemon);
+
+const sortOrdenar = (a, b) => {
+  return a - b;
+};
+pokemon.sort(sortOrdenar);
+console.log(pokemon.sort(sortOrdenar));
+
+console.log("--Fin Ejercicio 5 ");
