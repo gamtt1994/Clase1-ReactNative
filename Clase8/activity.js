dc851506a37a40ba8a3bd4b515a15cb5876d7712
Gustavo Maldonado <gamtt1994@gmail.com>
2020-10-15 20:44:23 -0500

    Moving

---
+++ Clase8/activity.js
@@ -0,0 +1,46 @@
+class roundFight {
+  constructor() {
+    this.createCharacter = function (type, name) {
+      let character;
+      if (type === "hero") character = new Hero(name);
+      else if (type == "villain") character = new Villain(name);
+
+      return character;
+    };
+  }
+}
+
+class Hero {
+  constructor(name) {
+    this._type = "hero";
+    this.name = name;
+    this.attack = function () {
+      return `The hero: ${name} this attacking`;
+    };
+    this.guard = function () {
+      return `The hero: ${name} this defending`;
+    };
+  }
+}
+
+class Villain {
+  constructor(name) {
+    this._type = "villain";
+    this.name = name;
+    this.attack = function () {
+      return `The villain: ${name} this attacking`;
+    };
+    this.guard = function () {
+      return `The villain: ${name} this defending`;
+    };
+  }
+}
+
+// creating objects
+const fight = new roundFight();
+
+const createHero = fight.createCharacter("hero", "Gustavo");
+const createVillain = fight.createCharacter("villain", "Alejandro");
+
+console.log("Moving Hero:", createHero.attack());
+console.log("Moving Villain", createVillain.attack());
